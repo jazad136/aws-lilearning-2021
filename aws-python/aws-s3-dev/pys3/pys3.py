a83fa983743287a26b9e5d5e0ed3e26aa960bdca
Jonathan A. Saddler <1129384+jazad136@users.noreply.github.com>
2021-11-10 21:16:00 -0500

    Added all functions

--- aws-python/aws-s3-dev/pys3/pys3.py
+++ aws-python/aws-s3-dev/pys3/pys3.py
@@ -11,6 +11,9 @@ SECRET_KEY = 'AWS_SECRET_ACCESS_KEY'
 REGION = 'us-east-2'
 PRI_BUCKET_NAME = 'jaslil'
 TRANSIENT_BUCKET_NAME = 'jaslil2'
+TEMPORARY_BUCKET_NAME = 'jastemp2'
+THIRD_BUCKET_NAME = 'jaslilv3'
+
 F1 = 'lil1.txt'
 F2 = 'lil2.txt'
 F3 = 'lil3.txt'
@@ -109,8 +112,34 @@ def create_bucket(name, s3, secure=False):
     except ClientError as ce:
         print('error', ce)
 
+def generate_download_link(bucket, key, expiration_seconds, s3):
+    try:
+        response = s3.meta.client.generate_presigned_url('get_object', Params = {
+            'Bucket': bucket,
+            'Key': key
+        }, ExpiresIn=expiration_seconds)
+        print(response)
+    except ClientError as ce:
+        print('error', ce)
+    
+def delete_bucket(bucket, s3):
+    try:
+        s3.Bucket(bucket).delete()
+    except ClientError as ce: 
+        print('error', ce)
+        
 def main():
     """entry point"""
+    '''
+    Create a temporary bucket
+    Upload a file
+    Copy a file from an existing bucket
+    Get the files and print them
+    Download a file
+    Generate a presigned URL
+    Delete the files
+    Delete the bucket
+    '''
     import boto3
     # might not be a good idea to upload these to git... 
     # storing these elsewhere now...
@@ -120,10 +149,33 @@ def main():
                         aws_access_key_id=access,
                         aws_secret_access_key=secret)
     
-    # these three methods work. 
+    #Create a temporary bucket
+    create_bucket(TEMPORARY_BUCKET_NAME, s3)
+    #Upload a file
+    upload_file(TEMPORARY_BUCKET_NAME, DIR, F1, s3)
+    #Copy a file
+    copy_file(PRI_BUCKET_NAME, TEMPORARY_BUCKET_NAME, F2, F2, s3)
+    #Get the files
+    print(list_objects(TEMPORARY_BUCKET_NAME, s3))
     
-    create_bucket(TRANSIENT_BUCKET_NAME, s3, True)
-
-    
+    #Download a file
+    download_file(TEMPORARY_BUCKET_NAME, DOWN_DIR, F1, F1, s3)
+    #Generate presigned URL
+    generate_download_link(TEMPORARY_BUCKET_NAME, F1, 300, s3)
+    #Delete the files
+    delete_files(TEMPORARY_BUCKET_NAME, [F1, F2], s3)
+    #Delete the bucket
+    delete_bucket(TEMPORARY_BUCKET_NAME, s3)
+    '''
+    What should happen?
+    Bucket jastemp should be created on my s3 instance. 
+    Bucket jastemp contains file F1 (it reads "This is file 1")
+    Bucket jastemp contains file F2 (it reads "This is file 2")
+    The contents of jastemp should be printed to contain F1 and F2.
+    The file F1 should be downloaded to s3-alt
+    A presigned URL should be printed to the console, and we should be able to use it to download F1
+    The files F1 and F2 should not exist in jastemp
+    The bucket jastemp should no longer exist. 
+    '''
 if __name__ == '__main__':
     main()
