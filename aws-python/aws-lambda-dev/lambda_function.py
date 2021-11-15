7310c66fcf1c85c72c161d1a1b2493c5d05578a1
Jonathan A. Saddler <1129384+jazad136@users.noreply.github.com>
2021-11-15 14:38:28 -0500

    Update createQuotePy to work on Lambda

--- aws-python/aws-lambda-dev/lambda_function.py
+++ aws-python/aws-lambda-dev/lambda_function.py
@@ -2,15 +2,19 @@ import json
 import uuid
 import boto3
 import os
+import json
+import uuid
+import boto3
+import os
 def create_quote(event, context):
     tableName = os.getenv('TABLE_NAME')
     dynamodb = boto3.client('dynamodb')
+    
     row = {
         'id': uuid.uuid1().hex,
         'Name': event['name']
     }
-    #quotesT = dynamodb.Table('quotes')
-    dynamodb.put_item(TableName=tableName, Item=row)
+    dynamodb.put_item(TableName=tableName, Item={'id':{'S':uuid.uuid1().hex},'name':{'S':event['name']}})
     return {
         'statusCode': 200,
         'body': json.dumps(str('Put [' + event['name'] + ']. Line 12'))
