43f1e8af37b5ce71f8d4298979cdd0db86be259b
jazad136 <126-saddlerj20@users.noreply.gitlab.cs.ecu.edu>
2022-01-12 09:18:56 -0500

    Added Java Create Bucket Procedure

--- aws-java/javas3/src/main/java/net/saddlercoms/lil/s3/Application.java
+++ aws-java/javas3/src/main/java/net/saddlercoms/lil/s3/Application.java
@@ -1,7 +1,60 @@
 package net.saddlercoms.lil.s3;
 
+import java.io.File;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProviderChain;
+import software.amazon.awssdk.auth.credentials.AwsSessionCredentials;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.CreateBucketRequest;
+
 public class Application {
-	public static void main(String[] args) {
-		System.out.println("hello world");
+	private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+	private static final String AWS_ACCESS_KEY = "AWS_ACCESS_KEY_ID";
+	private static final String AWS_SECRET_KEY = "AWS_SECRET_ACCESS_KEY";
+	private static final String PRI_BUCKET_NAME = "jaslil";
+	private static final String TRANSIENT_BUCKET_NAME = "jaslil2";
+
+	private static final String F1 = "lil1.txt";
+	private static final String F2 = "lil2.txt";
+	private static final String F3 = "lil3.txt";
+	private static final String BASE = "/Users/jsaddle/code/pers2/plain/AWS_exercise2/aws-s3-dev";
+	private static final File fBASE = new File("C:\\Users\\jsaddle\\code\\pers2\\plain\\AWS_exercise2\\aws-s3-dev");
+	private static final String DIR = BASE + "/s3";
+	private static final String DOWN_DIR = BASE + "/s3-alt";
+	
+	private final S3Client s3;
+	
+	public Application(S3Client s3) { 
+		this.s3 = s3;
 	}
+	
+	public void createBucket(String name) { 
+		try { 
+			CreateBucketRequest request = CreateBucketRequest
+					.builder()
+					.bucket(name)
+					.build();
+			s3.createBucket(request);
+		} catch(Exception e) { 
+			LOGGER.error("error during create bucket", e);
+		}
+	}
+	
+	public static void main(String[] args) {
+		String accessKey = System.getenv(AWS_ACCESS_KEY);
+		String secretKey = System.getenv(AWS_SECRET_KEY);
+		AwsSessionCredentials creds = AwsSessionCredentials.create(accessKey, secretKey, "");
+		
+		S3Client s3 = S3Client.builder().credentialsProvider(StaticCredentialsProvider.create(creds)).build();
+		Application app = new Application(s3);
+		app.createBucket(PRI_BUCKET_NAME);
+	}
+	
+	
 }
