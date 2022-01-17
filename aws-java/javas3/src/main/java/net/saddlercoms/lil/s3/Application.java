31eb5ff0a2f533c812bbf24d9d9c04b424ccef45
jazad136 <126-saddlerj20@users.noreply.gitlab.cs.ecu.edu>
2022-01-17 11:39:45 -0500

    Need Log4J Statement atop main to activate logging

--- aws-java/javas3/src/main/java/net/saddlercoms/lil/s3/Application.java
+++ aws-java/javas3/src/main/java/net/saddlercoms/lil/s3/Application.java
@@ -2,13 +2,11 @@ package net.saddlercoms.lil.s3;
 
 import java.io.File;
 
+import org.apache.log4j.BasicConfigurator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
-import software.amazon.awssdk.auth.credentials.AwsCredentialsProviderChain;
 import software.amazon.awssdk.auth.credentials.AwsSessionCredentials;
-import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.CreateBucketRequest;
@@ -18,7 +16,7 @@ public class Application {
 	private static final String AWS_ACCESS_KEY = "AWS_ACCESS_KEY_ID";
 	private static final String AWS_SECRET_KEY = "AWS_SECRET_ACCESS_KEY";
 	private static final String PRI_BUCKET_NAME = "jaslil";
-	private static final String TRANSIENT_BUCKET_NAME = "jaslil2";
+	private static final String TRANSIENT_BUCKET_NAME = "jaslilv2";
 
 	private static final String F1 = "lil1.txt";
 	private static final String F2 = "lil2.txt";
@@ -26,7 +24,7 @@ public class Application {
 	private static final String BASE = "/Users/jsaddle/code/pers2/plain/AWS_exercise2/aws-s3-dev";
 	private static final File fBASE = new File("C:\\Users\\jsaddle\\code\\pers2\\plain\\AWS_exercise2\\aws-s3-dev");
 	private static final String DIR = BASE + "/s3";
-	private static final String DOWN_DIR = BASE + "/s3-alt";
+	private static final String DOWN_DIR = BASE + "/s3alt";
 	
 	private final S3Client s3;
 	
@@ -47,11 +45,15 @@ public class Application {
 	}
 	
 	public static void main(String[] args) {
+		// this line must be here to configure Log4J to work. 
+		BasicConfigurator.configure();
+		
 		String accessKey = System.getenv(AWS_ACCESS_KEY);
 		String secretKey = System.getenv(AWS_SECRET_KEY);
 		AwsSessionCredentials creds = AwsSessionCredentials.create(accessKey, secretKey, "");
 		
-		S3Client s3 = S3Client.builder().credentialsProvider(StaticCredentialsProvider.create(creds)).build();
+		S3Client s3 = S3Client.builder()
+				.credentialsProvider(StaticCredentialsProvider.create(creds)).build();
 		Application app = new Application(s3);
 		app.createBucket(PRI_BUCKET_NAME);
 	}
